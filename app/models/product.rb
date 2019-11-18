65eac46a55d4f04ce621715f172d65b40c258bb9
Jules Douglas <jules.douglas@aceproject.space>
2019-11-18 16:01:30 -0600

    moved active admin stylesheets

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+  has_many :region_mades
+  has_many :province_mades
+  has_many :country_mades
+  has_many :wineries
+  has_many :varieties
   belongs_to :variety
 
   validates :designation, :description, :price, :stockDate, presence: true
