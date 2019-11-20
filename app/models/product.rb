3133ef4942d9fb39bad4388f6c344cc8224ef3cc
Jules Douglas <jules.douglas@aceproject.space>
2019-11-20 12:16:45 -0600

    updated images to page

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,7 +7,5 @@ class Product < ApplicationRecord
   belongs_to :winery
   belongs_to :variety
 
-  has_one_attached :image
-
   validates :designation, :description, :price, :stockDate, presence: true
 end
